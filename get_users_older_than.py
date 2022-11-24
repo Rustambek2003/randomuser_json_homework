b6704f9b9d05eb986f610d164760a5ad2412d2a5
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-24 17:03:33 +0500

    4

--- get_users_older_than.py
+++ get_users_older_than.py
@@ -1,3 +1,4 @@
+from get_users import read_json
 def get_users_older_than(data:dict, age:int)->list:
     """Gets all users older than a certain age from the data
     Args:
@@ -6,4 +7,13 @@ def get_users_older_than(data:dict, age:int)->list:
     Returns:
         list: A list of users
     """
-    pass
+    ans = []
+    data = data['users']
+    for i in data:
+        if i['age'] >= age:
+            i = i['name']
+            ans.append(f"{i['title']} {i['first']} {i['last']}")
+    return ans
+
+data = read_json('users.json')
+print(get_users_older_than(data,25))
