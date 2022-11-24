78d88486ad828b2ed6f582421cda8a628b99c0df
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-24 17:07:13 +0500

    1

--- get_male_users_from_country.py
+++ get_male_users_from_country.py
@@ -12,15 +12,12 @@ def get_male_users_from_country(data:dict, country:str)->list:
     ans = []
     data = data['users']
     for i in data:
-        if i['gender'] == "male":
+        if i['gender'] == "male" and i['country']==country:
             i = i['name']
             ans.append(f"{i['title']} {i['first']} {i['last']}")
     return ans
 data = read_json("users.json")
-count = data['users']
-country = ''
-for i in count:
-    country += i['country'] + ' '
-print(get_male_users_from_country(data,country))
+
+print(get_male_users_from_country(data,"USA"))
 
     
