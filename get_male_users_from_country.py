f1703764bb114d1dd84549125c30f010cb3a545f
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-24 16:57:42 +0500

    1

--- get_male_users_from_country.py
+++ get_male_users_from_country.py
@@ -1,3 +1,4 @@
+from get_users import read_json
 def get_male_users_from_country(data:dict, country:str)->list:
     """
     Get male users from a country from the data
@@ -8,6 +9,18 @@ def get_male_users_from_country(data:dict, country:str)->list:
 
         list: A list of users
     """ 
-    pass
+    ans = []
+    data = data['users']
+    for i in data:
+        if i['gender'] == "male":
+            i = i['name']
+            ans.append(f"{i['title']} {i['first']} {i['last']}")
+    return ans
+data = read_json("users.json")
+count = data['users']
+country = ''
+for i in count:
+    country += i['country'] + ' '
+print(get_male_users_from_country(data,country))
 
     
